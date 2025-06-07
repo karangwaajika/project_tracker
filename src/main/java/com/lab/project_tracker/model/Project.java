195a65aedfb81810f32ef896b7763bad535c1447
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-06-07 11:53:17 +0200

    feat: create project model

---
+++ src/main/java/com/lab/project_tracker/model/Project.java
@@ -0,0 +1,39 @@
+package com.lab.project_tracker.model;
+
+import com.lab.project_tracker.util.ProjectStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "project")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private LocalDate deadline;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ProjectStatus status;
+
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
---
+++ src/main/java/com/lab/project_tracker/util/ProjectStatus.java
@@ -0,0 +1,7 @@
+package com.lab.project_tracker.util;
+public enum ProjectStatus {
+    NOT_STARTED,
+    IN_PROGRESS,
+    OVERDUE,
+    COMPLETED
+}
