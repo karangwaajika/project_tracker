8c55bba9eaa24efef554b8e183ce1c04803b10ac
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-06-07 13:09:36 +0200

    doc: document API end points using swagger

--- src/main/java/com/lab/project_tracker/controller/ProjectController.java
+++ src/main/java/com/lab/project_tracker/controller/ProjectController.java
@@ -2,6 +2,8 @@ package com.lab.project_tracker.controller;
 
 import com.lab.project_tracker.model.Project;
 import com.lab.project_tracker.service.ProjectService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("projects")
+@Tag(name = "Project Controller", description = "Manage all the Project's urls")
 public class ProjectController {
     ProjectService projectService;
 
@@ -19,6 +22,8 @@ public class ProjectController {
     }
 
     @PostMapping(name = "add_project", path = "/add")
+    @Operation(summary = "Create project",
+            description = "This request inserts a project to the database and returns the inserted project ")
     public ResponseEntity<Project> addProject(@RequestBody Project project){
         Project savedProject = this.projectService.create(project);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedProject);
