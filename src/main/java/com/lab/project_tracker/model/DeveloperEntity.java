0ab686d8deb65c52c88ceb2fefa2eae60f188ee6
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-06-08 11:10:06 +0200

    feat: create a many-to-many table for developer and skill

---
+++ src/main/java/com/lab/project_tracker/model/DeveloperEntity.java
@@ -0,0 +1,33 @@
+package com.lab.project_tracker.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "developer")
+public class DeveloperEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+
+    @ManyToMany
+    @JoinTable(
+            name = "developer_skill",
+            joinColumns = @JoinColumn(name = "developer_id"),
+            inverseJoinColumns = @JoinColumn(name = "skill_id")
+    )
+    private Set<SkillEntity> skills = new HashSet<>();
+}
---
+++ src/main/java/com/lab/project_tracker/model/SkillEntity.java
@@ -0,0 +1,27 @@
+package com.lab.project_tracker.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "skill")
+public class SkillEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "skills")
+    private Set<DeveloperEntity> developers = new HashSet<>(); // no duplicate
+}
